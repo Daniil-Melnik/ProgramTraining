0a080be676c4846a868455625fc183fa0bb0fbbf
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-06-19 14:11:10 +0300

    added tokenisation

--- src/main/java/testing/from11to20/ValueOfAnArithmeticExpression_18.java
+++ src/main/java/testing/from11to20/ValueOfAnArithmeticExpression_18.java
@@ -5,6 +5,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Set;
 
 public class ValueOfAnArithmeticExpression_18 {
     public static void main(String[] args) throws IOException {
@@ -16,8 +20,42 @@ public class ValueOfAnArithmeticExpression_18 {
 
         ExpressionParser parser = new ExpressionParser(s);
 
+        System.out.println(Arrays.toString(stringToTokens(s)));
+
         reader.close();
         writer.close();
+    }
+
+    private static String[] stringToTokens(String s){
+        int pos = 0;
+        LinkedList<String> listOfTokens = new LinkedList<>();
+
+        String pS = s.replaceAll(" ", "");
+
+        Set<Character> brackets = Set.of('(', ')');
+        Set<Character> operators = Set.of('-', '+', '*', '/');
+
+        while (pos != s.length()){
+            System.out.println(pos + "/" + s.length());
+            char currCh = s.charAt(pos);
+            if (brackets.contains(currCh)){
+                listOfTokens.add(String.valueOf(currCh));
+                pos++;
+            } else if (operators.contains(currCh)) {
+                listOfTokens.add(String.valueOf(currCh));
+                pos++;
+            } else if (Character.isDigit(currCh)) {
+                StringBuilder numberBuilder = new StringBuilder();
+                while (Character.isDigit(currCh)){
+                    numberBuilder.append(currCh);
+                    pos++;
+                    currCh = pS.charAt(pos);
+                }
+                listOfTokens.add(numberBuilder.toString());
+            }
+        }
+
+        return listOfTokens.toArray(new String[0]);
     }
 
     private static class ExpressionParser{
