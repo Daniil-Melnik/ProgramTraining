da826c0dd5880981c4b6630610e1b4849e6b0bc2
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-06-19 17:46:41 +0300

    rework with 18

--- src/main/java/testing/from11to20/ValueOfAnArithmeticExpression_18.java
+++ src/main/java/testing/from11to20/ValueOfAnArithmeticExpression_18.java
@@ -5,8 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Set;
 
@@ -15,30 +13,31 @@ public class ValueOfAnArithmeticExpression_18 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String s = reader.readLine().replaceAll(" ", "");
-        System.out.println(s);
+        String s = reader.readLine();
+        //System.out.println(Arrays.toString(stringToTokens(s)));
 
-        ExpressionParser parser = new ExpressionParser(s);
-
-        System.out.println(Arrays.toString(stringToTokens(s)));
+        try {
+            ExpressionParser parser = new ExpressionParser(stringToTokens(s));
+            writer.write(String.valueOf((int) parser.parse()));
+        } catch (IllegalStateException e){
+            writer.write("WRONG");
+        }
 
         reader.close();
         writer.close();
     }
 
-    private static String[] stringToTokens(String s){
+    private static String[] stringToTokens(String pS){
         int pos = 0;
         LinkedList<String> listOfTokens = new LinkedList<>();
 
-        String pS = s.replaceAll(" ", "");
-
         Set<Character> brackets = Set.of('(', ')');
-        Set<Character> operators = Set.of('-', '+', '*', '/');
+        Set<Character> operators = Set.of('-', '+', '*');
 
-        while (pos != s.length()){
-            System.out.println(pos + "/" + s.length());
-            char currCh = s.charAt(pos);
-            if (brackets.contains(currCh)){
+        while (pos < pS.length()){
+            char currCh = pS.charAt(pos);
+            if (currCh == ' ') pos++;
+            else if (brackets.contains(currCh)){
                 listOfTokens.add(String.valueOf(currCh));
                 pos++;
             } else if (operators.contains(currCh)) {
@@ -49,9 +48,14 @@ public class ValueOfAnArithmeticExpression_18 {
                 while (Character.isDigit(currCh)){
                     numberBuilder.append(currCh);
                     pos++;
+
+                    if (pos >= pS.length()) break;
+
                     currCh = pS.charAt(pos);
                 }
                 listOfTokens.add(numberBuilder.toString());
+            } else {
+                throw new IllegalStateException("Некорректный символ - " + currCh);
             }
         }
 
@@ -59,27 +63,106 @@ public class ValueOfAnArithmeticExpression_18 {
     }
 
     private static class ExpressionParser{
-        private String [] tokens; // пересмотреть на строку или пересмотреть токенизацию
+        private String [] tokens;
         private int pos = 0;
 
-        public ExpressionParser(String s){
-            tokens = s.split("");
+        public ExpressionParser(String [] t){
+            tokens = t;
         }
 
-        public Double parse(){
-            return 0D;
+        public double parse() throws IllegalStateException{
+            if (tokens.length != 0) {
+                double result = expression();
+                if (pos != tokens.length) throw new IllegalStateException("Использованы не все токены!");
+                return result;
+            } else throw new IllegalStateException("Пустая строка на входе");
         }
 
-        public Double expression(){
-            return 0D;
+        public double expression(){
+            double left = term();
+
+            while (pos < tokens.length){
+                String op = tokens[pos];
+                if (!op.equals("+") && !op.equals("-")){
+                    break;
+                } else {
+                    pos++;
+                }
+
+                double right = term();
+
+                if (op.equals("+")) left += right;
+                else left -= right;
+            }
+
+            return left;
         }
 
-        public Double term(){
-            return 0D;
+        public double term(){
+            double left = factor();
+
+            while (pos < tokens.length){
+                String op = tokens[pos];
+                if (!((op.equals("*") || op.equals("/")))){
+                    break;
+                } else pos++;
+
+                double right = factor();
+
+                if (op.equals("*")) left*= right;
+                else {
+                    throw new IllegalStateException("Деление");
+                    /*if (right != 0){
+                        left /= right;
+                    } else throw new IllegalStateException("Деление на 0");*/
+
+                }
+            }
+            return left;
         }
 
-        public Double factor(){
-            return 0D;
+        public double factor() throws IllegalStateException{
+            if (pos >= tokens.length) throw new IllegalStateException("Неожиданный конец выражения");
+
+            String next = tokens[pos];
+            double result;
+
+            if (next.equals("-")) {
+                pos++;
+                double val = factor();
+                return -val;
+            }
+
+            if (next.equals("+")) {
+                pos++;
+                return factor();   // унарный плюс ничего не меняет
+            }
+
+            if (next.equals("(")){
+                pos++;
+                result = expression();
+                String endBracket = "";
+                if (pos < tokens.length && (endBracket = tokens[pos]).equals(")")){
+                    pos++;
+                    return result;
+                } else {
+                    throw new IllegalStateException("Ожидалась закрывающая скобка, а не " + endBracket);
+                }
+            } else if (!isNumber(tokens[pos]))
+                throw new IllegalStateException("Ожидалось число, а получено " + tokens[pos]);
+            else {
+                pos++;
+                return Double.parseDouble(next);
+            }
+        }
+
+        private boolean isNumber(String s){
+            try {
+                Integer.parseInt(s);
+                return true;
+            } catch (NumberFormatException e){
+                return false;
+            }
         }
     }
 }
