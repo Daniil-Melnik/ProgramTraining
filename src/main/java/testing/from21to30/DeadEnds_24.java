273c30e3b3665724a8802a23e2814b542125f383
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-06-30 21:16:43 +0300

    started 24

---
+++ src/main/java/testing/from21to30/DeadEnds_24.java
@@ -0,0 +1,19 @@
+package testing.from21to30;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class DeadEnds_24 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        
+
+        reader.close();
+        writer.close();
+    }
+}
